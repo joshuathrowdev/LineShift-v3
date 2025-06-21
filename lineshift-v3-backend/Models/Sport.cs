cf81a7258bdf7ca5fb0fc78f00f88c55f48f9ddb
Joshua <joshuathrow.dev@gmail.com>
2025-06-21 15:42:19 -0500

    setup and initailize the ApplicationDbContext file with the Sports Tabl Config

--- lineshift-v3-backend/Models/Sport.cs
+++ lineshift-v3-backend/Models/Sport.cs
@@ -1,9 +1,13 @@
-﻿namespace lineshift_v3_backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lineshift_v3_backend.Models
 {
     public class Sport
     {
+        [Key]
         public int? SportId { get; set; }
 
+        [Required]
         public string? SportName { get; set; }
     }
 }
