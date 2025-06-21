ebbdc221868ef1a14601007870accaeef18f9ff3
Joshua <joshuathrow.dev@gmail.com>
2025-06-21 17:06:35 -0500

    Intialize migrations folder and register Sports Service with builder

--- lineshift-v3-backend/Program.cs
+++ lineshift-v3-backend/Program.cs
@@ -1,4 +1,11 @@
 
+using lineshift_v3_backend.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using lineshift_v3_backend.Services;
+using lineshift_v3_backend.DataAccess.Repository;
+using Microsoft.Extensions.Options;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace lineshift_v3_backend
 {
     public class Program
@@ -9,8 +16,42 @@ namespace lineshift_v3_backend
 
             // Add services to the container.
             // Any dependencies or transative dependencies to controller (services and repos)
+            builder.Services.AddSingleton<ISportsService, SportsService>();
+
 
             // Add the DBContext (Connection/Connection String) to MariaDB
+
+                // 1. Get the connection String from appsettings.json
+            var connectionString = builder.Configuration.GetConnectionString("MariaDbConnection")
+                ?? throw new InvalidOperationException("Connection string 'MariaDbConnection' not found ");
+
+                // 2. Configure our DbContext for MariaDB
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                // 3. Specify the server version of our MariaDB instance
+                // we could explicity state the server version if we know (better for prod env)
+                // EX: var serverVersion = new MariaDbServerVersion(new Version(10, 11, 2))
+                var serverVersion = new MariaDbServerVersion(new Version(11, 8, 2));
+                options.UseMySql(connectionString, serverVersion, mySqlOptions =>
+                {
+                    mySqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 10,
+                        maxRetryDelay: TimeSpan.FromSeconds(30),
+                        errorNumbersToAdd: null
+                     );
+
+                    // Optional: Configure specific data type mappings
+                    // This is useful if you want to explicitly control how certain C# types map to MariaDB types.
+                    // For example, mapping C# bool to MariaDB TINYINT(1) (default for Pomelo) or BIT(1)
+                    // mySqlOptions.DefaultDataTypeMappings(mappings => mappings.WithClrBoolean(MySqlBooleanType.Bit1));
+
+                    // Optional: Configure behavior for schema (MySQL/MariaDB don't have EF Core schemas)
+                    // mySqlOptions.SchemaBehavior(MySqlSchemaBehavior.Ignore); // Or .Throw()
+                });
+            });
+
+            // Add other services (Authentication, Authorization, etc.)
+            // ...
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +59,9 @@ namespace lineshift_v3_backend
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            // --- MIDDLEWARE PIPELINE (app.Use...) ---
+            // This is where request processing middleware is ordered.
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
--- lineshift-v3-backend/Services/SportsService.cs
+++ lineshift-v3-backend/Services/SportsService.cs
@@ -1,6 +1,44 @@
-﻿namespace lineshift_v3_backend.Services
+﻿using lineshift_v3_backend.Models;
+
+namespace lineshift_v3_backend.Services
 {
-    public class SportsService
+    #region Service Interface
+    public interface ISportsService
     {
+        Task<ICollection<Sport>> GetSports();
+    }
+    #endregion
+    public class SportsService : ISportsService
+    {
+        // Layer Vars
+        private readonly ISportsService _sportsService;
+
+        // DI constructor for vars
+        public SportsService(ISportsService sportsService)
+        {
+            _sportsService = sportsService;
+        }
+
+        #region Methods
+        public async Task<ICollection<Sport>> GetSports()
+        {
+            try
+            {
+                // Call Next Layer (Repository Layer) (Actual LINQ Code base)
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+
+        #region Interface Implementation
+        Task<ICollection<Sport>> ISportsService.GetSports()
+        {
+            return GetSports();
+        }
+        #endregion
     }
 }
