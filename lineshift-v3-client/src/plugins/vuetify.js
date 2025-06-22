b0c2403ffb5e3e8ac968656725f14b964dc4c865
Joshua <joshuathrow.dev@gmail.com>
2025-06-22 10:45:55 -0500

    configure the main brand theme and add some logos

--- lineshift-v3-client/src/plugins/vuetify.js
+++ lineshift-v3-client/src/plugins/vuetify.js
@@ -11,9 +11,30 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+// Custom Defined Themes 
+const mainTheme = {
+  dark: true,
+  colors: {
+    background: '#292927', // default background for theme
+    surface: '#212121', // default background for surface components (ex: cards) (slightly darker)
+    primary: '#22577A',
+    secondary: '#57CC99',
+    ternary: '#80ED99',
+    altOne: '#38A3A5',
+    altTwo: '#C7F9CC',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00'
+  }
+}
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'mainTheme',
+    themes: {
+      mainTheme,
+    }
   },
 })
