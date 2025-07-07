3d040241a81316d5821f06d55b9228ad66dcfdad
Joshua <joshuathrow.dev@gmail.com>
2025-07-06 19:30:39 -0500

    reconfigure the theme plugin to include variant tones for veutify variants

--- lineshift-v3-client/components.d.ts
+++ lineshift-v3-client/components.d.ts
@@ -9,12 +9,9 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AccountAvatar: typeof import('./src/components/navigation/account/AccountAvatar.vue')['default']
-    AccountCard: typeof import('./src/components/navigation/account/AccountCard.vue')['default']
     AccountInformationChip: typeof import('./src/components/navigation/account/AccountInformationChip.vue')['default']
     AccountSection: typeof import('./src/components/navigation/account/AccountSection.vue')['default']
-    AccoutSection: typeof import('./src/components/navigation/account/AccountSection.vue')['default']
     AnnouncementBanner: typeof import('./src/components/home/AnnouncementBanner.vue')['default']
-    AppBar: typeof import('./src/components/navigation/AppBar.vue')['default']
     LoginForm: typeof import('./src/components/authentication/LoginForm.vue')['default']
     NavLinkItem: typeof import('./src/components/navigation/NavLinkItem.vue')['default']
     NavLinks: typeof import('./src/components/navigation/NavLinks.vue')['default']
